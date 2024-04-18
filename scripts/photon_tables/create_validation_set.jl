bcd9bd416b27f6a13fb9b942fe44395caad7655c
Christian Haack <chr.hck@gmail.come>
2024-04-18 11:25:12 +0200

    Refactor create_validation_set.jl script to use ProgressBars and ArgParse

--- scripts/photon_tables/create_validation_set.jl
+++ scripts/photon_tables/create_validation_set.jl
@@ -6,14 +6,9 @@ using Distributions
 using PhysicsTools
 using JLD2
 using Base.Iterators
-rng = MersenneTwister(42)
-hbc, hbg = make_hit_buffers()
-
-wl_range = (300.0f0, 800.0f0)
-
-g = 0.95f0
-target = POM(SA_F32[0, 0, 0], UInt16(1))
-
+using ProgressBars
+using ArgParse
+import Base.GC: gc
 
 function sample_position_sphere(rng, max_radius)
     r = sqrt(rand(rng, Uniform(0, max_radius^2)))
@@ -34,56 +29,83 @@ function sample_uniform_direction(rng)
 end
 
 
+function main(parsed_args)
 
-workdir = ENV["ECAPSTOR"]
-outfile_c = joinpath(workdir, "surrogate_validation_sets/cascades.jdl2")
-jldopen(outfile_c, "w") do file
+    jldopen(parsed_args[:o], "w") do file
+    end
+
+    rng = MersenneTwister(parsed_args[:s])
+    hbc, hbg = make_hit_buffers()
+    wl_range = (300.0f0, 800.0f0)
+    g = 0.95f0
+    target = POM(SA_F32[0, 0, 0], UInt16(1))
+
+    ptype = parsed_args[:mode] ==:lightsabre ? PMuPlus : PEPlus
+    plength = parsed_args[:mode] ==:lightsabre ? 1E4 : 0.
+    lst = parsed_args[:mode] ==:lightsabre ? FastLightsabreMuonEmitter : ExtendedCherenkovEmitter
+
+    for _ in 1:parsed_args[:n]
+        position = sample_position_sphere(rng, 100)
+        direction = sample_uniform_direction(rng)
+        energy = 10 .^ rand(rng, Uniform(2, 6))
+
+        particle = Particle(position, direction, 0., energy, plength, ptype)
+        particle = convert(Particle{Float32}, particle)
+
+        abs_scale = Float32(1 + randn(rng) * 0.05)
+        sca_scale = Float32(1 + randn(rng) * 0.05)
+        medium = make_cascadia_medium_properties(g, abs_scale, sca_scale)
+        spectrum = make_cherenkov_spectrum(wl_range, medium)
+        
+        lightsource = lst(particle, medium, spectrum)
+
+        setup = PhotonPropSetup(lightsource, target, medium, spectrum, 1, photon_scaling=parsed_args[:photon_scaling])
+
+        photons = propagate_photons(setup, hbc, hbg, copy_output=false)
+        if nrow(photons) == 0
+            continue
+        end
+        hits = make_hits_from_photons(photons, setup)
+        if nrow(hits) == 0
+            continue
+        end
+        calc_pe_weight!(hits, setup)
+
+        jldopen(parsed_args[:o], "r+") do file
+            dataset_id =  length(file) + 1
+            grp = JLD2.Group(file, "dataset_$dataset_id")
+            grp["hits"] = hits
+            grp["particle"] = particle
+            grp["abs_scale"] = abs_scale
+            grp["sca_scale"] = sca_scale
+        end
+    end
 end
 
-outfile_t = joinpath(workdir, "surrogate_validation_sets/tracks.jdl2")
-jldopen(outfile_t, "w") do file
+mode_choices = ["extended", "lightsabre"]
+
+s = ArgParseSettings()
+@add_arg_table s begin
+    "-o"
+    help = "Output file"
+    required = true
+    "-n"
+    help = "Number of events"
+    required = true
+    arg_type = Int64
+    "--mode"
+    help = "Simulation Mode;  must be one of " * join(mode_choices, ", ", " or ")
+    range_tester = (x -> x in mode_choices)
+    required = true
+    "-s"
+    help = "Seed"
+    arg_type = Int64
+    required = true
+    "--photon_scaling"
+    help = "Oversampling ratio"
+    arg_type = Float64
+    default = 1.
 end
+parsed_args = parse_args(ARGS, s; as_symbols=true)
 
-types = [:track, :cascade]
-
-for (i, t) in product(1:200, types)
-    position = sample_position_sphere(rng, 100)
-    direction = sample_uniform_direction(rng)
-    energy = 10 .^ rand(rng, Uniform(2, 6))
-
-    ptype = t==:track ? PMuPlus : PEPlus
-    plength = t==:track ? 1E4 : 0.
-
-    particle = Particle(position, direction, 0., energy, plength, ptype)
-    particle = convert(Particle{Float32}, particle)
-
-    abs_scale = Float32(1 + randn(rng) * 0.05)
-    sca_scale = Float32(1 + randn(rng) * 0.05)
-    medium = make_cascadia_medium_properties(g, abs_scale, sca_scale)
-    spectrum = make_cherenkov_spectrum(wl_range, medium)
-
-    lst = t==:track ? FastLightsabreMuonEmitter : ExtendedCherenkovEmitter
-    lightsource = lst(particle, medium, spectrum)
-
-    setup = PhotonPropSetup(lightsource, target, medium, spectrum, 1)
-
-    photons = propagate_photons(setup, hbc, hbg, copy_output=true)
-    if nrow(photons) == 0
-        continue
-    end
-    hits = make_hits_from_photons(photons, setup)
-    if nrow(hits) == 0
-        continue
-    end
-    calc_pe_weight!(hits, setup)
-    outfile = t==:track ? outfile_t : outfile_c
-
-    jldopen(outfile, "r+") do file
-        dataset_id =  length(file) + 1
-        grp = JLD2.Group(file, "dataset_$dataset_id")
-        grp["hits"] = hits
-        grp["particle"] = particle
-        grp["abs_scale"] = abs_scale
-        grp["sca_scale"] = sca_scale
-    end
-end
+main(parsed_args)
