410bcd39bf43c01d1802dde7c94bc18400cd2563
Christian Haack <chr.hck@gmail.come>
2024-06-11 10:25:59 +0200

    update plotting

--- examples/plot_lightyields.jl
+++ examples/plot_lightyields.jl
@@ -87,42 +87,6 @@ lines(group_velocity.(lambdas, Ref(medium)))
 
 
 
-medium = make_cascadia_medium_properties(0.95)
-wl_range = (300., 800.)
-spectrum = make_cherenkov_spectrum(wl_range, medium)
-
-using StatsBase
-using CurveFit
-data = []
-log_energies = 1.:0.5:7
-lys = Float64[]
-tlen = 1000.
-for le in log_energies
-    particle = Particle(SA_F64[0, 0, 0], SA_F64[0, 0, 1], 0., 10^le, tlen, PMuMinus)
-    for i in 1:500
-        p, secondaries = propagate_muon(particle)
-        if length(secondaries) > 0
-            ly_secondaries = sum(total_lightyield.(secondaries, Ref(medium), Ref(spectrum)))
-        else
-            ly_secondaries = 0.
-        end
-        push!(lys, ly_secondaries)
-    end
-    ly_secondaries = mean(lys) / tlen
-    push!(data, (log_energy=le, mean_ly=ly_secondaries))
-end
-
-data = DataFrame(data)
-fig, ax, lin = lines(data[:, :log_energy], log10.(data[:, :mean_ly]))
-coeffs = poly_fit(data[:, :log_energy], log10.(data[:, :mean_ly]), 5)
-lines!(ax, log_energies, Polynomial(coeffs).(log_energies))
-fig
-fig, ax, lin = lines(data[:, :log_energy], (data[:, :mean_ly]))
-lines!(ax, log_energies, 10 .^Polynomial(coeffs).(log_energies))
-fig
-
-
-coeffs
 
 dom = DOM(SA[0., 0., 0.], 1)
 
@@ -205,26 +169,3 @@ fig
 
 
 
-
-
-em_energy = sum([s.energy for s in secondaries if s.type == PEMinus])
-
-scaled_energy = 
-
-log_energies = 1.:0.5:7
-lys = Float64[]
-tlen = 1000.
-for le in log_energies
-    particle = Particle(SA_F64[0, 0, 0], SA_F64[0, 0, 1], 0., 10^le, tlen, PMuMinus)
-    for i in 1:500
-        p, secondaries = propagate_muon(particle)
-        if length(secondaries) > 0
-            ly_secondaries = sum(total_lightyield.(secondaries, Ref(medium), Ref(wl_range)))
-        else
-            ly_secondaries = 0.
-        end
-        push!(lys, ly_secondaries)
-    end
-    ly_secondaries = mean(lys) / tlen
-    push!(data, (log_energy=le, mean_ly=ly_secondaries))
-end
